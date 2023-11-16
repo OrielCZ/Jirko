3818eb13fe01ae34aeb6a953faaca0e47656fce9
OrielCZ <ondra.zach@gmail.com>
2023-11-16 17:44:50 +0100

    Už to skoro je

--- Program.cs
+++ Program.cs
@@ -1,4 +1,22 @@
-﻿string input = "Jirko! ?";
+﻿using System;
+using System.Media;
+using System.Numerics;
+
+static void Main(string[] args)
+{
+    PlayMusic("Jirko.wav");
+    Console.WriteLine("Plaing music");
+    Console.ReadLine();
+}
+
+static void PlayMusic(string filepath)
+{
+    SoundPlayer musicPlaer = new SoundPlayer();
+    musicPlaer.SoundLocation = filepath;
+    musicPlaer.Play();
+}
+/*
+string input = "Jirko! ?";
 double i = 100000;
 
 for (int j = 0; j < i; j++)
@@ -6,3 +24,4 @@ for (int j = 0; j < i; j++)
     Console.Write(input + " ");
     Thread.Sleep(100);
 }
+*/
