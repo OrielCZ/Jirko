1c08e65b40d7f52ab811586148ba25033b63cd8b
OrielCZ <ondra.zach@gmail.com>
2023-11-17 09:27:30 +0100

    Mluvící Jirka

--- Program.cs
+++ Program.cs
@@ -2,19 +2,32 @@
 using System.Media;
 using System.Numerics;
 
-static void Main(string[] args)
+
+namespace Jirko
 {
-    PlayMusic("Jirko.wav");
-    Console.WriteLine("Plaing music");
-    Console.ReadLine();
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlayMusic("Jirko.wav");
+            Console.WriteLine("Plaing music");
+        
+        }
+
+        public static void PlayMusic(string filepath)
+        {   
+            SoundPlayer musicPlaer = new SoundPlayer();
+            musicPlaer.SoundLocation = filepath;
+            musicPlaer.PlaySync();
+                
+        }
+    }
+
 }
 
-static void PlayMusic(string filepath)
-{
-    SoundPlayer musicPlaer = new SoundPlayer();
-    musicPlaer.SoundLocation = filepath;
-    musicPlaer.Play();
-}
+
 /*
 string input = "Jirko! ?";
 double i = 100000;
